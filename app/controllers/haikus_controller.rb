633ce945fe15c9c9617b9f9647188ffc5ce2cd3e
Avidor Turkewitz <aviturkewitz@gmail.com>
2015-11-28 12:25:08 -0500

    Put Top Haikus page into bootstrap columns

--- app/controllers/haikus_controller.rb
+++ app/controllers/haikus_controller.rb
@@ -4,7 +4,7 @@ class HaikusController < ApplicationController
   # GET /haikus
   # GET /haikus.json
   def index
-    @haikus = Haiku.order(likes: :desc).limit(10)
+    @haikus = Haiku.order(likes: :desc).limit(6)
   end
 
   def comparison
