0e414c7f9030811dde15a612e5b45b1654f6ff09
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-01-04 11:11:52 -0500

    Haikus not saving with proper user ID and user cannot find their haikus

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  before_action :set_user
+
   def show
+    @auhtored = @user.haikus
   end
+
+  private
+
+    def set_user
+      @user = current_user
+    end
 end
